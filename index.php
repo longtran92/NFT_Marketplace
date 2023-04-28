a471ee8ed89bc7af8ee2e55d58c1636263857183
longtran92 <xuanlongtran@gmail.com>
2023-04-28 10:37:08 +0700

    Stash for auction-section fix

--- index.php
+++ index.php
@@ -647,6 +647,72 @@
               See NFT
             </button>
           </div>
+          <div class="timeRemaining-card">
+            <span class="heading">Auction ends in:</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="howItWorks-section">
+    <div class="container">
+      <div class="section-content">
+        <h3 class="section-heading">How It Works</h3>
+        <span class="section-subHeading">Find out how to get started</span>
+        <div class="cards-container">
+          <div class="card">
+            <img src="./images/Homepage/how-it-works/card-1.png" alt="" class="card-img">
+            <div class="txt-container">
+              <h5 class="card-heading">Setup Your wallet</h5>
+              <p class="card-description">Set up your wallet of choice. Connect it to the Animarket by clicking the
+                wallet icon in the top right corner.</p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="./images/Homepage/how-it-works/card-2.png" alt="" class="card-img">
+            <div class="txt-container">
+              <h5 class="card-heading">Create Collection</h5>
+              <p class="card-description">Upload your work and setup your collection. Add a description, social links
+                and floor price.</p>
+            </div>
+          </div>
+          <div class="card">
+            <img src="./images/Homepage/how-it-works/card-3.png" alt="" class="card-img">
+            <div class="txt-container">
+              <h5 class="card-heading">Start Earning</h5>
+              <p class="card-description">Choose between auctions and fixed-price listings. Start earning by selling
+                your NFTs or trading others.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="newsSubscription-section">
+    <div class="container">
+      <div class="subscription-card">
+        <div class="card-left">
+          <img src="./images/Homepage/weekly-update.png" alt="" class="card-img">
+        </div>
+        <div class="card-right">
+          <h3 class="card-heading">Join our weekly digest</h3>
+          <span class="card-description">Get exclusive promotions & updates straight to your inbox.</span>
+          <div class="subscribe-form">
+            <input type="text" placeholder='Enter your email here'>
+            <button class="btn-primary btn-subscribe">
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M1.875 4.375C1.875 4.02982 2.15482 3.75 2.5 3.75H17.5C17.8452 3.75 18.125 4.02982 18.125 4.375V15C18.125 15.3315 17.9933 15.6495 17.7589 15.8839C17.5245 16.1183 17.2065 16.25 16.875 16.25H3.125C2.79348 16.25 2.47554 16.1183 2.24112 15.8839C2.0067 15.6495 1.875 15.3315 1.875 15V4.375ZM3.125 5V15H16.875V5H3.125Z"
+                  fill="white" />
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M2.03928 3.95268C2.27253 3.69823 2.66788 3.68104 2.92233 3.91428L10 10.4021L17.0777 3.91428C17.3321 3.68104 17.7275 3.69823 17.9607 3.95268C18.194 4.20713 18.1768 4.60248 17.9223 4.83573L10.4223 11.7107C10.1834 11.9298 9.81663 11.9298 9.57768 11.7107L2.07768 4.83573C1.82323 4.60248 1.80604 4.20713 2.03928 3.95268Z"
+                  fill="white" />
+              </svg>
+              Subscribe
+            </button>
+          </div>
         </div>
       </div>
     </div>
