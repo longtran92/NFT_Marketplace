21b5dbe04d91e0d4151a475fecf057e438b0dece
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-05-05 22:00:34 +0700

    create profile

---
+++ profile.php
@@ -0,0 +1,137 @@
+<?php include './templates/head.php'; ?>
+<div class="container-wrapper">
+    <div class="header">
+        <div class="logo">
+            <a href="#">
+                <img src="./images/logo.png" alt="" class="logo-header">
+            </a>
+        </div>
+        <div class="menu-wrapper">
+            <ul class="menu-header">
+                <li>
+                    <a href="#">Marketplace</a>
+                </li>
+                <li>
+                    <a href="#">
+                        Rankings
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Connect a wallet
+                    </a>
+                </li>
+            </ul>
+
+            <button class="btn-sign-up">
+
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M10 3.125C7.58375 3.125 5.625 5.08375 5.625 7.5C5.625 9.91625 7.58375 11.875 10 11.875C12.4162 11.875 14.375 9.91625 14.375 7.5C14.375 5.08375 12.4162 3.125 10 3.125ZM4.375 7.5C4.375 4.3934 6.8934 1.875 10 1.875C13.1066 1.875 15.625 4.3934 15.625 7.5C15.625 10.6066 13.1066 13.125 10 13.125C6.8934 13.125 4.375 10.6066 4.375 7.5Z"
+                        fill="white" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M10.0001 13.124C8.57374 13.124 7.17251 13.4995 5.93728 14.2128C4.70205 14.926 3.67634 15.9518 2.96327 17.1872C2.79071 17.4861 2.40848 17.5886 2.10953 17.416C1.81058 17.2435 1.70812 16.8612 1.88068 16.5623C2.70345 15.1369 3.88696 13.9532 5.31223 13.1303C6.7375 12.3073 8.3543 11.874 10.0001 11.874C11.6459 11.874 13.2627 12.3073 14.688 13.1303C16.1132 13.9532 17.2968 15.1369 18.1195 16.5623C18.2921 16.8612 18.1896 17.2435 17.8907 17.416C17.5917 17.5886 17.2095 17.4861 17.0369 17.1872C16.3239 15.9518 15.2982 14.926 14.0629 14.2128C12.8277 13.4995 11.4265 13.124 10.0001 13.124Z"
+                        fill="white" />
+                </svg>
+
+                <span>Sign Up</span>
+            </button>
+        </div>
+    </div>
+
+
+</div>
+<div class="banner-wrapper"></div>
+<div class="container-wrapper">
+    <div class="container-content">
+        <div class="profile-avatar">
+            <img src="./images/avatar.png" alt="" class="avatar">
+        </div>
+
+        <div class="profile-info">
+            <div class="profile-info__header">
+                <h1 class="username">Animakid</h1>
+                <div class="list-btn">
+                    <button class="btn btn-image">
+
+                        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M6.75 3.125C6.75 2.77982 7.02982 2.5 7.375 2.5H17.375C17.7202 2.5 18 2.77982 18 3.125V13.125C18 13.4702 17.7202 13.75 17.375 13.75H13.625C13.2798 13.75 13 13.4702 13 13.125C13 12.7798 13.2798 12.5 13.625 12.5H16.75V3.75H8V6.875C8 7.22018 7.72018 7.5 7.375 7.5C7.02982 7.5 6.75 7.22018 6.75 6.875V3.125Z"
+                                fill="white" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M3 6.875C3 6.52982 3.27982 6.25 3.625 6.25H13.625C13.9702 6.25 14.25 6.52982 14.25 6.875V16.875C14.25 17.2202 13.9702 17.5 13.625 17.5H3.625C3.27982 17.5 3 17.2202 3 16.875V6.875ZM4.25 7.5V16.25H13V7.5H4.25Z"
+                                fill="white" />
+                        </svg>
+
+                        <span>0xc0E3...B79C</span>
+                    </button>
+                    <button class="btn btn-follow">
+
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M2.5 10C2.5 9.65482 2.77982 9.375 3.125 9.375H16.875C17.2202 9.375 17.5 9.65482 17.5 10C17.5 10.3452 17.2202 10.625 16.875 10.625H3.125C2.77982 10.625 2.5 10.3452 2.5 10Z"
+                                fill="#A259FF" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M10 2.5C10.3452 2.5 10.625 2.77982 10.625 3.125V16.875C10.625 17.2202 10.3452 17.5 10 17.5C9.65482 17.5 9.375 17.2202 9.375 16.875V3.125C9.375 2.77982 9.65482 2.5 10 2.5Z"
+                                fill="#A259FF" />
+                        </svg>
+
+                        <span>Follow</span>
+                    </button>
+                </div>
+            </div>
+
+            <div class="profile-info__statics">
+                <div class="profile-info__item">
+                    <h3 class="profile-info__item-title">250+</h3>
+                    <span class="profile-info__item-label">Volume</span>
+                </div>
+                <div class="profile-info__item">
+                    <h3 class="profile-info__item-title">350+</h3>
+                    <span class="profile-info__item-label">NFTs Sold</span>
+                </div>
+                <div class="profile-info__item">
+                    <h3 class="profile-info__item-title">450+</h3>
+                    <span class="profile-info__item-label">Followers</span>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- TABS -->
+        <div id="tabs">
+            <ul class="list-tab">
+                <li><a href="#tabs-1">
+                        <span class="label">Created</span>
+                        <span class="count">242</span>
+                    </a></li>
+                <li><a href="#tabs-2">
+                        <span class="label">owned</span>
+                        <span class="count">242</span>
+                    </a></li>
+                <li><a href="#tabs-3">
+                        <span class="label">Follow</span>
+                        <span class="count">242</span>
+                    </a></li>
+            </ul>
+            <div id="tabs-1">
+                <p>tab1</p>
+            </div>
+            <div id="tabs-2">
+                <p>tab2</p>
+            </div>
+            <div id="tabs-3">
+                tab3
+            </div>
+        </div>
+
+
+    </div>
+</div>
+<script type="text/javascript" src="./js/jquery.min.js"></script>
+<script type="text/javascript" src="./js/jquery-ui.min.js"></script>
+<script>
+$(function() {
+    $("#tabs").tabs();
+});
+</script>
--- templates/head.php
+++ templates/head.php
@@ -2,13 +2,14 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- <link rel='stylesheet' href="css/bootstrap.min.css"> -->
-  <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css">
-  <link rel=" stylesheet" href="css/header.css">
-  <link rel="stylesheet" href="css/home-1.css">
-  <link rel="stylesheet" href="css/footer-1.css">
-  <title>NFL Marketplace</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel='stylesheet' href="css/bootstrap.min.css"> -->
+    <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css">
+    <link rel=" stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/home-1.css">
+    <link rel="stylesheet" href="css/profile.css">
+    <link rel="stylesheet" href="css/footer-1.css">
+    <title>NFL Marketplace</title>
 </head>
